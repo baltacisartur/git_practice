8f7f9d27ad33e2848cf49e0df1dbcf461e600762
bojarin <baltacisartur@gmail.com>
2026-05-21 11:14:14 +0200

    new_job

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+
+def func(num: int):
+    seen = {}
+    numbers = list(random.randint(1, 10) for _ in range(num))
+    for i in numbers:
+        if i not in seen:
+            seen[i] = 1
+        else:
+            seen[i] += 1
+    
+    return [key for key, value in seen.items() if value > 1], numbers
+
+s = 10
+print(func(s))
